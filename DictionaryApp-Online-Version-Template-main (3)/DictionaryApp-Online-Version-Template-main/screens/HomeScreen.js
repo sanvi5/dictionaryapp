7b57dc7a6d7ef76ecc33275f302637f1e7b87279
sanvi5 <124884991+sanvi5@users.noreply.github.com>
2023-08-17 17:06:28 +0530

    Update HomeScreen.js

--- DictionaryApp-Online-Version-Template-main (3)/DictionaryApp-Online-Version-Template-main/screens/HomeScreen.js
+++ DictionaryApp-Online-Version-Template-main (3)/DictionaryApp-Online-Version-Template-main/screens/HomeScreen.js
@@ -17,7 +17,7 @@ export default class HomeScreen extends Component{
   getWord=(word)=>{
     var searchKeyword=word.toLowerCase()
     var url;
-    //url = "https://rupinwhitehatjr.github.io/dictionary/searchKeyword.json"
+    url = "https://rupinwhitehatjr.github.io/dictionary/searchKeyword.json"
     //url = "https://rupinwhitehatjr.github.io/dictionary/"+ +".json"
     //url = "https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
     //url = "https://rupinwhitehatjr.github.io/dictionary/"+word+".json"
